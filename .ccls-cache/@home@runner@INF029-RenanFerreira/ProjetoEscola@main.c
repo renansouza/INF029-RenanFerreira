82508668c9b33da2ac4b675c5c3db255ef1a6d38
Renan Souza <falecomrenansouza@gmail.com>
2023-03-17 21:46:11 +0000

    Programa modificado

--- .ccls-cache/@home@runner@INF029-RenanFerreira/ProjetoEscola@main.c
+++ .ccls-cache/@home@runner@INF029-RenanFerreira/ProjetoEscola@main.c
@@ -2,19 +2,27 @@
 #include <stdlib.h>
 #include "main.h"
 
-int main() 
-{
-
 void menu(opcao){
-  int opcao;
+  
   do{
 
     printf("\nMENU ESCOLAR\n\n1- Alunos\n2 - Docentes\n\nESCOLHA UMA OPCAO: ");
     scanf("%d", &opcao);
-   
+
+  
+  }
+    
   }while(opcao != 0);
 
+  
+  
+
 }
+
+int main() 
+{
+
+
 
 menu(0);
   
